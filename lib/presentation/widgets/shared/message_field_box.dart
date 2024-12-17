c30a4c6f8e23b8f689338c65e93c6a9102709f0c
Javier Moreno <jmoreno@sintad.pe>
2024-12-17 18:49:51 -0500

    74 - TextFormField

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textController = TextEditingController();
+
+    final outlineInputBorder = UnderlineInputBorder(
+      borderSide: const BorderSide(color:Colors.transparent),
+      borderRadius: BorderRadius.circular(40)
+    );
+
+    final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "??"',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        icon: const Icon(Icons.send_outlined),
+        onPressed: (){
+          print('Valor de la caja de texto');
+        },
+      )
+    );
+
+    return TextFormField(
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('submit value $value');
+      },
+      onChanged: (value) {
+        print('changed: $value');
+      },
+    );
+  }
+}
