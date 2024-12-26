7fb505eafeda1e9fad8abe5442d4293318d54e80
Javier Moreno <jmoreno@sintad.pe>
2024-12-26 16:37:20 -0500

    Clase 90.- Tarea - Mensajes de ella y Clase 91.- Codigo fuente de la seccion | Fin de la Seccion 6

--- lib/presentation/widgets/chat/her_message_bubble.dart
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
 
 class HerMessageBubble extends StatelessWidget {
-  const HerMessageBubble({super.key});
+  final Message message;
+
+  const HerMessageBubble({
+    super.key,
+    required this.message
+  });
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +21,19 @@ class HerMessageBubble extends StatelessWidget {
           decoration: BoxDecoration(
               color: colors.secondary,
               borderRadius: BorderRadius.circular(20)),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Text(
-              'Hola Mundo',
-              style: TextStyle(color: Colors.white),
+              message.text,
+              style: const TextStyle(color: Colors.white),
             ),
           ),
         ),
         const SizedBox(height: 5),
 
-        _ImageBubble(),
+        _ImageBubble(message.imageUrl!),
 
-        SizedBox(height: 10),
+        const SizedBox(height: 10),
         //Todo: imagen
       ],
     );
@@ -34,6 +41,10 @@ class HerMessageBubble extends StatelessWidget {
 }
 
 class _ImageBubble extends StatelessWidget {
+
+  final String imageUrl;
+
+  const _ImageBubble( this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +55,7 @@ class _ImageBubble extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
-          'https://yesno.wtf/assets/yes/1-af11222d8d4af90bdab8fc447c8cfebf.gif',
+          imageUrl,
           width: size.width*0.7,
           height: 150,
           fit: BoxFit.cover,
