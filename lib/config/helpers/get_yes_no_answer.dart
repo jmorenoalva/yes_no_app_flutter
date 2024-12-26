06e230d89f75038bc4d94982d9613e654dd71214
Javier Moreno <jmoreno@sintad.pe>
2024-12-26 16:24:27 -0500

    Clase 89.- QuickType.io al rescate

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -12,10 +12,6 @@ class GetYesNoAnswer {
 
     final yesNoModel = YesNoModel.fromJsonMap(response.data);
 
-    return Message(
-      text: yesNoModel.answer,
-      fromWho: FromWho.hers,
-      imageUrl: yesNoModel.image
-    );
+    return yesNoModel.toMessageEntity();
   }
 }
